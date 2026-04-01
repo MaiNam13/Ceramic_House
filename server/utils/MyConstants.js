f1cca294bc071be4085e563b68e03ddf487be281
HoThiPhuongThao <hothiphuongthao1002@gmail.com>
2026-04-01 14:43:19 +0700

    fix data

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,11 +1,13 @@
 const MyConstants = {
-  DB_SERVER: 'shop.rj08n.mongodb.net',
-  DB_USER: 'hothiphuongthao1002',
-  DB_PASS: 'phuongthao10022005',
-  DB_DATABASE: 'shoppingonline',
+  DB_SERVER: 'cluster0.xqmg3rg.mongodb.net',
+  DB_USER: 'hothiphuongthao1002_db_user',
+  DB_PASS: '55AqCPv3vGW998k5',
+  DB_DATABASE: 'Ceramic',
   EMAIL_USER: 'hothiphuongthao1002@gmail.com', 
-  EMAIL_PASS: 'xfrb xmhv vaor opbp',
+  EMAIL_PASS: 'jhli ukgr hwsa jdku',
   JWT_SECRET: 'thao123456',
   JWT_EXPIRES : 60 * 60 * 1000, // in milliseconds
 };
 module.exports = MyConstants ;
+
+
